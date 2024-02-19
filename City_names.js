9d4ae4af53fc25e4b252aab2f97485b6a4e17008
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:47:39 -0800

    Add files via upload

---
+++ City_names.js
@@ -0,0 +1,12 @@
+"use strict";
+//Written by : TAYYABA RAO.
+// Date : 18/feb/2024
+//Roll no: 00388416
+//function for city_country
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+//result
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "france"));
---
+++ City_names.ts
@@ -0,0 +1,13 @@
+//Written by : TAYYABA RAO.
+// Date : 18/feb/2024
+//Roll no: 00388416
+
+//function for city_country
+function city_country(city:string, country: string): string{
+    return `${city}, ${country}`;
+}
+
+//result
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "france"));
